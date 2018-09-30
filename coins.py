3e3e1539973ab56997824c4497a445be2d0ef4f8
E-man <emanuelh77@hotmail.com>
2018-09-29 20:33:55 -0600

    updated

--- coins.py
+++ coins.py
@@ -1,3 +1,4 @@
+<<<<<<< current
 import random
 
 class Coin:
@@ -28,7 +29,7 @@ class Coin:
     def __del__(self):
             print('Coin spent!')
 
-        
+
     def flip(self):
             heads_options = [True, False]
             choice = random.choice(heads_options)
@@ -37,7 +38,7 @@ class Coin:
     def __str__(self):
         return '{} cent Coin'.format(int(self.original_value*100))
 
-    
+
 class Quarter(Coin):
 
     def __init__(self):
@@ -63,7 +64,7 @@ class Dime(Coin):
             'rusty_color':'greenish',
             'num_edges': 1,
             'diameter': 17.91,
-            'thickness': 1.35, 
+            'thickness': 1.35,
             'mass': 2.268
             }
         super().__init__(**data)
@@ -78,7 +79,7 @@ class Nickel(Coin):
             'rusty_color':'greenish',
             'num_edges': 1,
             'diameter': 21.21,
-            'thickness': 1.95, 
+            'thickness': 1.95,
             'mass': 5.00
             }
         super().__init__(**data)
@@ -93,7 +94,7 @@ class Penny(Coin):
             'rusty_color': None,
             'num_edges': 1,
             'diameter': 19.05,
-            'thickness': 1.55, 
+            'thickness': 1.55,
             'mass': 2.50
             }
         super().__init__(**data)
@@ -103,6 +104,116 @@ coins = [Penny(),Nickel(),Dime(),Quarter()]
 for coin in coins:
     arguments = [coin, coin.color, coin.value, coin.diameter, coin.thickness,
                  coin.num_edges, coin.mass]
-    
+
     string = '{} - color: {}, value: {}, diameter(mm): {}, thickness(mm): {}, # of edges: {}, mass(g): {}'.format(*arguments)
     print(string)
+=======
+import random
+
+class Coin:
+    def __init__(self, rare=False, clean=True, heads = True, **kwargs):
+
+        for key,value in kwargs.items():
+            setattr(self,key,value)
+
+        self.is_rare = rare
+        self.is_clean = clean
+        self.heads = heads
+        if self.is_rare:
+            self.value = self.original_value * 1.25
+        else:
+            self.value = self.original_value
+
+        if self.is_clean:
+            self.color = self.clean_color
+        else:
+            self.color = self.rusty_color
+
+    def rust(self):
+            self.color = self.rusty_color
+
+    def clean(self):
+            self.color = self.clean_color
+
+    def __del__(self):
+            print('Coin spent!')
+
+
+    def flip(self):
+            heads_options = [True, False]
+            choice = random.choice(heads_options)
+            self.heads = choice
+
+    def __str__(self):
+        return '{} cent Coin'.format(int(self.original_value*100))
+
+
+class Quarter(Coin):
+
+    def __init__(self):
+
+        data = {
+            'original_value': .25,
+            'clean_color':'silver',
+            'rusty_color':'greenish',
+            'num_edges': 1,
+            'diameter': 24.26,
+            'thickness': 1.75,
+            'mass': 5.67
+            }
+        super().__init__(**data)
+
+class Dime(Coin):
+
+    def __init__(self):
+
+        data = {
+            'original_value': .10,
+            'clean_color':'silver',
+            'rusty_color':'greenish',
+            'num_edges': 1,
+            'diameter': 17.91,
+            'thickness': 1.35,
+            'mass': 2.268
+            }
+        super().__init__(**data)
+
+class Nickel(Coin):
+
+    def __init__(self):
+
+        data = {
+            'original_value': .05,
+            'clean_color':'silver',
+            'rusty_color':'greenish',
+            'num_edges': 1,
+            'diameter': 21.21,
+            'thickness': 1.95,
+            'mass': 5.00
+            }
+        super().__init__(**data)
+
+class Penny(Coin):
+
+    def __init__(self):
+
+        data = {
+            'original_value': .01,
+            'clean_color':'copper',
+            'rusty_color': None,
+            'num_edges': 1,
+            'diameter': 19.05,
+            'thickness': 1.55,
+            'mass': 2.50
+            }
+        super().__init__(**data)
+
+coins = [Penny(),Nickel(),Dime(),Quarter()]
+
+for coin in coins:
+    arguments = [coin, coin.color, coin.value, coin.diameter, coin.thickness,
+                 coin.num_edges, coin.mass]
+
+    string = '{} - color: {}, value: {}, diameter(mm): {}, thickness(mm): {}, # of edges: {}, mass(g): {}'.format(*arguments)
+    print(string)
+>>>>>>> before discard
